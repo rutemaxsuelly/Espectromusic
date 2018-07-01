7b7211138cbe41ab8e815efbd20abb98b6257460
Mychellne <msh@cin.ufpe.be>
2018-07-01 19:00:49 -0300

    Revert "Revert "Modificações nos scripts e materiais da cena""
    This reverts commit 6fa4cd6f0b6bf3a9260c3995fc06bfdb97272b61.

--- Assets/Scripts/InstaciarCubos.cs
+++ Assets/Scripts/InstaciarCubos.cs
@@ -12,14 +12,14 @@ public class InstaciarCubos : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        for (int i = 0; i < 512; i++)
+        for (int i = 0; i < 50; i++)
         {
 
             GameObject _instanceSampleCube = (GameObject)Instantiate(_SampleCuberPrefab);
             _instanceSampleCube.transform.position = this.transform.position;
             _instanceSampleCube.transform.parent = this.transform;
             _instanceSampleCube.name = "SampleCube" + i;
-            this.transform.position = new Vector3(i % 50 * 15, 0, i/50 * 20);
+            this.transform.position = new Vector3(i % 10 * 15, 0, i/5 * 20);
             _instanceSampleCube.transform.position = Vector3.forward * 10;
             _sampleCube[i] = _instanceSampleCube;
         }
@@ -29,7 +29,7 @@ public class InstaciarCubos : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < 512; i++){
+        for (int i = 0; i < 50; i++){
             if (_sampleCube != null) { 
                 _sampleCube[i].transform.localScale = new Vector3 (10,(AudioPeer._samples[i] * _maxscale)+ 2 , 10);
             }

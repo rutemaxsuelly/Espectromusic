9bb73a8bae0d4bff07e81c91939ac84dce36fa6a
Jáder Anderson Oliveira de Abreu <jaderxnet@gmail.com>
2018-07-01 15:55:59 -0300

    - Junção de controlador de jogador com chamada de LibPD - Controle de movimento com setas direcionais - Controle de salto com espaço

--- Assets/Scripts/ControladorJogador.cs
+++ Assets/Scripts/ControladorJogador.cs
@@ -1,23 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LibPDBinding;
 
+/**
+ * @jaderxnet
+ * Esta classe é responsável por controlar o jogador
+ * */
 public class ControladorJogador : MonoBehaviour {
+	//Corpo Rígido associado ao objeto controlado pelo jogador
     public Rigidbody rb;
-    public float speed;
+	//Fator de Velocidade usado para movimentações
+	public float speedFactor = 100f;
+	//Velocidade máxma do jogador
+	public float maxSpeedXZ = 5f;
+	//Velocidade máxma do jogador
+	public float maxSpeedJump = 10f;
 
-
+	//Fator usado para saltos
+	public float jumpFactor = 10f;
+	//Escala maxima de notas geradas
+	public float scale = 100f;
+	//Frequência básica para notas geradas
+	private int basicFrequency = 60;
+	private bool isJump = false;
+	//Inicialíza as variáveis não inializadas
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
+
+	//Atualizações fixas
     void FixedUpdate()
     {
+		//Captura a entrada de controle Horizontal, usando por padrão setas direcionais direita e esquerda e retornando valores de -1 a 1;
         float moveHorizontal = Input.GetAxis("Horizontal");
+		//Captura a entrada de controle Vertical, usando por padrão setas direcionais para cima e para baixo e retornando valores entre -1 e 1; 
         float moveVertical = Input.GetAxis("Vertical");
+		//Captura a entrada de salto, usando por padrão o espaço e retornano 0 ou 1;
+		float jump = Input.GetAxis("Jump");
 
-        Vector3 movement = new Vector3(moveHorizontal,0, moveVertical);
+		//Atualiza as velociades Horizontal e Vertical
+		float speedH = moveHorizontal * speedFactor * Time.deltaTime;
+		float speedV = moveVertical * speedFactor * Time.deltaTime;
+		rb.velocity = new Vector3 (speedH, rb.velocity.y, speedV);
 
-        rb.velocity = movement * speed;
+		//Aplica uma velocidade vertical se um pulo for executado
+		if (!isJump && jump > 0) {
+			isJump = true;
+			rb.velocity = new Vector3(rb.velocity.x, jump * jumpFactor, rb.velocity.z);
+		}
+
+		//Se o jogador chegar perto do chão, já pode pular novamente
+		if (rb.position.y < 1) {
+			isJump = false;
+		}
     }
+		
+	//Atualizações dinâmicas
+	void Update () {
+		//Toca uma nota de acordo com a magnitude da velocidade
+		float pitch = GetPitch(rb.velocity.sqrMagnitude);
+		LibPD.SendFloat ("bounce", pitch);
+	}
+
+	//Retorna um float calculado com base na frequencia basica, aceleração e escala
+	float GetPitch (float aceleracao) {
+		return scale * aceleracao + basicFrequency;
+	}
 }

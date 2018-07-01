07d7586715650c90de514872d781c81ac134a74a
Mychellne <msh@cin.ufpe.be>
2018-07-01 18:04:04 -0300

    Modificações nos scripts e materiais da cena
    - Material da bola (Amarelo) e cubo( Vermelho)
    - No Scritpt AudioPeer foi modificado de _sample para _samples
    - O Script InstanciarCubos possui outros valores para teste de cenários
    - O Script ParamCube só faz a alteração do Spectrum de um cubo se duplicarmos esse objeto.

--- Assets/Scripts/AudioPeer.cs
+++ Assets/Scripts/AudioPeer.cs
@@ -4,27 +4,53 @@ using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
 
-public class AudioPeer : MonoBehaviour
-{
+public class AudioPeer : MonoBehaviour{ 
     AudioSource _audioSource;
-    public static float[] _sample = new float[512];
+    public static float[] _samples = new float[512];
+    public static float[] _FreqBand = new float[8];
+
 
     // Use this for initialization
     void Start()
     {
-        _audioSource = GetComponent<AudioSource>();
+        _audioSource = GetComponent<AudioSource> ();
     }
 
     // Update is called once per frame
     void Update()
     {
         GetSpectrumAudioSource();
+        MakeFrequencyBands();
     }
 
     void GetSpectrumAudioSource()
     {
-        _audioSource.GetSpectrumData(_sample, 0, FFTWindow.Blackman);
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
     }
 
+    void MakeFrequencyBands()
+    {
+        int count = 0;
 
+        for (int i = 0; i < 8; i++){
+
+            float average = 0;
+
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 7)
+            {
+                sampleCount += 2;
+
+            }
+            for (int j = 0; j < sampleCount; j++) { 
+            
+                average += _samples [count] * (count + 1);
+                count++;
+            }
+            average /= count;
+
+            _FreqBand[i] = average * 10;
+        }
+    }
 }

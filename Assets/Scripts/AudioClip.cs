c5d1c453d9498b3a427557f6816d034a721a906c
Rute Maxsuelly <rutymaxsuelly@gmail.com>
2018-07-04 13:22:01 -0300

    Criação de Fase2
    Melhorias na posição da câmera em relação ao jogador;
    Organização de cenas;
    Testes iniciais microfone por AudioClip;
    Scripts de PdMicrofone para o jogador controlar o espectro;

---
+++ Assets/Scripts/AudioClip.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Esta classe é para capturar microfone e gerar musica pelo instrumentista;
+public class AudioClip : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

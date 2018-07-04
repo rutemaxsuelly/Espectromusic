c1b4b5300d2b421dabfd99e04b81a15249191540
Mychellne <msh@cin.ufpe.be>
2018-07-04 12:49:04 -0300

    Tela inicial, nomes atualizados e fase 1
    - Mudei a tela inicial ( personagem agora é uma "bomba", coloquei uma sugestão de nome para o jogo "BomberMusic".
    - Inclui um cubo na fase 1 (AScene) para ser um sensor que leva a bolinha para outra fase, ainda estou fazendo os testes.
    - Renomeei a pasta onde fica os canos e os nomes das paredes

---
+++ Assets/Scripts/Sensor_Fase2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sensor_Fase2 : MonoBehaviour {
+    void OntriggerEnter(Collider hit)
+    {
+
+        if (hit.tag == "Player") ;
+        {
+
+            SceneManager.LoadScene(3);
+
+        }
+    }
+
+}

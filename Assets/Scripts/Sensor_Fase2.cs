c73e12734b2918e886e7aed76636794c12a73501
jaderxnet <jaderxnet@gmail.com>
2018-07-04 23:16:51 -0300

    Correção de mudança de Lével
    Modifiquei o T do "OnTriggerEnter" que estava minúsculo e adicionei nas configurações de Build a Cena da fase 2 com indice 2.

--- Assets/Scripts/Sensor_Fase2.cs
+++ Assets/Scripts/Sensor_Fase2.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Sensor_Fase2 : MonoBehaviour {
-    void OntriggerEnter(Collider hit)
+    void OnTriggerEnter(Collider hit)
     {
+		Debug.Log (" Colidiu com " + hit.tag);
 
-        if (hit.tag == "Player") ;
+        if (hit.tag == "Play") ;
         {
 
-            SceneManager.LoadScene(3);
+			SceneManager.LoadScene(2);
 
         }
     }

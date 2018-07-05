9e6dbe1021edaaf0faaa0f2f1b372cf2fb08b13a
Rute Maxsuelly <rutymaxsuelly@gmail.com>
2018-07-04 22:34:16 -0300

    Microfone Resolvido
    Implementação de AudioPeer com if no audioSettings para pegar audio do microfone;

--- Assets/Scripts/AudioPeer.cs
+++ Assets/Scripts/AudioPeer.cs
@@ -9,10 +9,35 @@ public class AudioPeer : MonoBehaviour
     AudioSource _audioSource;
     public static float[] _samples = new float[512];
 
+    //Microphone input
+    public AudioClip _audioClip;
+    public bool _useMicrophone;
+    public string _selectedDevice;
+
     // Use this for initialization
     void Start()
     {
         _audioSource = GetComponent<AudioSource>();
+
+        //Microfone input
+        if (_useMicrophone)
+        {
+            if (Microphone.devices.Length > 0)
+            {
+                _selectedDevice = Microphone.devices[0].ToString();
+                _audioSource.clip = Microphone.Start(_selectedDevice, true, 100, AudioSettings.outputSampleRate);
+            }
+            else
+            {
+                _useMicrophone = false;
+            }
+        }
+        if (!_useMicrophone)
+        {
+            _audioSource.clip = _audioClip;
+        }
+
+        _audioSource.Play();
     }
 
     // Update is called once per frame
@@ -25,6 +50,5 @@ public class AudioPeer : MonoBehaviour
     {
         _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
     }
-
 
 }
